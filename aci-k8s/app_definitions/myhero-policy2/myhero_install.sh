d7a1c684cb4f197d0f42a6daa8975176de3505c9
hpreston <hank.preston@gmail.com>
2018-08-01 15:57:56 -0400

    added ui install

--- aci-k8s/app_definitions/myhero-policy2/myhero_install.sh
+++ aci-k8s/app_definitions/myhero-policy2/myhero_install.sh
@@ -14,9 +14,10 @@ kubectl -n myhero apply -f myhero_data.yaml
 kubectl -n myhero apply -f myhero_mosca.yaml
 kubectl -n myhero apply -f myhero_ernst.yaml
 kubectl -n myhero apply -f myhero_app.yaml
-#
-# MYHERO_APP_EXTERNAL_IP=$(kubectl get services myhero-app -o json | python -c 'import sys, json; print json.load(sys.stdin)["status"]["loadBalancer"]["ingress"][0]["ip"]')
-# # echo "AP IP: ${MYHERO_APP_EXTERNAL_IP}"
-# sed "s/<PROVIDE-EXTERNAL-IP-FOR-myhero-app>/${MYHERO_APP_EXTERNAL_IP}/" myhero_ui.yaml > myhero_ui_ready.yaml
-#
-# kubectl apply -f myhero_ui_ready.yaml
+
+MYHERO_APP_EXTERNAL_IP=$(kubectl -n myhero get services myhero-app -o json | python -c 'import sys, json; print json.load(sys.stdin)["status"]["loadBalancer"]["ingress"][0]["ip"]')
+# echo "AP IP: ${MYHERO_APP_EXTERNAL_IP}"
+sed "s/kubesbxXX/kubesbx${POD_NUM}/" myhero_ui.template > myhero_ui.yaml
+sed "s/<PROVIDE-EXTERNAL-IP-FOR-myhero-app>/${MYHERO_APP_EXTERNAL_IP}/" myhero_ui.yaml > myhero_ui_ready.yaml
+
+kubectl -n myhero apply -f myhero_ui_ready.yaml
